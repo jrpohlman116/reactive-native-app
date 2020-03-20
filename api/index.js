8e0052f35fb5a902d3d3d8c315647d399606c771
Jordan Pohlman <jordanpohlman@gmail.com>
2020-03-19 19:54:34 -0500

    Add random restaurant generator by current location

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import { API_KEY } from 'react-native-dotenv'
+
+export function getRestaurantsAPI(lat, long) {
+    return fetch('https://developers.zomato.com/api/v2.1/search?lat=' + lat + '&lon=' + long + '&radius=16093.4', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-key': API_KEY
+        }
+    })
+    .then(response => response.json())
+}
